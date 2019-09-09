f7accf390f350ee9bd41d76175de0683beca7313
J <a@s.com>
2019-09-09 09:57:39 +0100

    First

---
+++ js/pub.js
@@ -0,0 +1,123 @@
+// alert("welcome to the king and tinker")
+// let age=prompt("how old are you")
+// parseInt(age)
+// let gender=prompt("What is your gender m/f")
+
+// // if(age.= 18 && gender=="f"){
+// //     alert('you get in free")
+// // }
+
+// // else (if age, 18){
+// //     else if(age >= 77 gender =="m"){
+// //         alert("£10 please")
+// //     }
+// // }
+
+// // age(" 18>=25 free"){
+
+// // }
+// // alert("f"){
+
+// // }
+// // age==("26>=35"){
+
+// // }
+// // alert("£5"){
+
+// // }
+// // age==("36+"){
+
+// // }
+// // aletr("£40"){
+
+// // }
+// // gender("male"){
+
+// // }
+
+// if(gender=="f"){
+//     if(age>=18 && age <=25){
+//     alert("free")
+//     }
+    
+//     else if(age>=26 && age <=35){
+//         alert("£5")
+//     }
+        
+//     else if(age >= 36){
+//         alert('£40')
+//     }
+// }
+
+// if(gender=="m"){
+//     if(age>=18 && age <=25){
+//         alert("£10")
+//     }
+    
+//     else if(age>=26 && age <=35){
+//         alert("£40")
+//     }
+        
+//     else if(age >= 36){
+//         alert('no')
+//     }    
+
+let number = prompt("enter number")
+
+if (number % 3 == 0){
+    alert("divisible by 3")
+    }
+    else if (number % 5 == 0){
+        alert("divisible by 5")
+    }else if (number % 5 == 0 && number % 3 ==  0  ){
+        alert("  divisible by 3 and 5")
+    }else {
+        alert("not divisible by 3 or 5")
+
+    }
+
+
+//     }
+//      alert("yes"){
+
+//     }
+
+
+
+//     if("num"){
+
+//     }
+//     else(num "x & 3"){
+
+//     }
+// name ("log"){
+
+// }
+
+let number = prompet("num"){
+
+}
+
+
+    if number("palindrome"){
+
+}
+   else(" == forward"){
+
+}
+   else("forward == backwards"){
+
+}
+   else("e.g. 1001 || 20202"){
+
+}
+
+
+
+
+
+     
+
+
+
+
